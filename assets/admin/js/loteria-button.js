35e857bbd90e9c953d8ea28c95d288b2fcde8db0
Edson Araujo (Mike) <edsomichael@gmail.com>
2023-11-10 00:02:33 -0300

    Update css and js loterias

--- assets/admin/js/loteria-button.js
+++ assets/admin/js/loteria-button.js
@@ -1,11 +1,42 @@
 (function() {
 
+    const switchModal = () => {
+        const modal = document.querySelector('.modalLoteriasPlugin')
+        const actualStyle = modal.style.display
+        if(actualStyle == 'block') {
+          modal.style.display = 'none'
+        }
+        else {
+          modal.style.display = 'block'
+        }
+      }
+      
+    //   const btn = document.querySelector('.modalBtn')
+    //   btn.addEventListener('click', switchModal)
+      
+      window.onclick = function(event) {
+          const modal = document.querySelector('.modalLoteriasPlugin')
+        if (event.target == modal) {
+          switchModal()
+        }
+      }
+
     var divModalLoterias = document.createElement("div");
     divModalLoterias.classList.add("modalLoteriasPlugin");
 
     var divModalLoteriasContent = document.createElement("div");
     divModalLoteriasContent.classList.add("content");
     divModalLoterias.appendChild(divModalLoteriasContent);
+
+    var divModalLoteriasHeader = document.createElement("h3");
+    divModalLoteriasHeader.innerText = "Selecione a loteria";
+    divModalLoteriasContent.appendChild(divModalLoteriasHeader);
+
+    var divModalLoteriasCheckbox = document.createElement("input");
+    divModalLoteriasCheckbox.type = "checkbox";
+    divModalLoteriasCheckbox.id = "modal_loteria_checkbox";
+    divModalLoteriasContent.appendChild(divModalLoteriasCheckbox);
+
 
     const divModalLoteriasSelect = document.createElement("select");
 
@@ -18,6 +49,12 @@
         
     });
 
+    var divModalLoteriasCheckboxLabel = document.createElement("label");
+    divModalLoteriasCheckboxLabel.innerText = "Exibir ultimo concurso";
+    divModalLoteriasCheckboxLabel.htmlFor = "modal_loteria_checkbox";
+    divModalLoteriasContent.appendChild(divModalLoteriasCheckboxLabel);
+
+
     divModalLoteriasContent.appendChild(divModalLoteriasSelect);
     document.body.appendChild(divModalLoterias);
 
@@ -25,9 +62,7 @@
         editor.addButton('loteria_button', {
             text: 'Inserir Loteria',
             icon: 'loteria-icon',
-            onclick: function() {
-                editor.insertContent('[loteria concurso="5" tipo_concurso="megasena"]');
-            }
+            onclick: switchModal
         });
     });
 })();
