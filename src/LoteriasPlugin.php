5bcf90b7e381aaf98b90060f6f2747ca90225d3b
Edson Araujo (Mike) <edsomichael@gmail.com>
2023-11-11 18:48:22 -0300

    Add enqueue css and js posts and pages Update rule to cache Create default cache latest

--- src/LoteriasPlugin.php
+++ src/LoteriasPlugin.php
@@ -12,11 +12,13 @@ class LoteriasPlugin {
 
     protected int $id_post;
     protected string $data_post;
+    protected int $default_time_transient = 1800;
 
     public function __construct() {
         
         add_action('init', [$this, 'init']);
         add_shortcode('loteria', [$this, 'loteriaShortcode']);
+        add_action( 'wp_enqueue_scripts', [$this, 'loteriaShortcodeScripts']);
 
     }
 
@@ -28,6 +30,14 @@ class LoteriasPlugin {
 
         $loteriasPostType->register();
         $loteriasTaxonomy->register();
+    }
+
+    function loteriaShortcodeScripts() {
+        global $post;
+        if( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'loteria') ) {
+            wp_enqueue_style( 'loterias-shortcode-plugin',  plugin_dir_url(__DIR__) . 'assets/css/loteria.css', array(), "1.0", 'all' );
+            wp_enqueue_script( 'loterias-shortcode-plugin',  plugin_dir_url(__DIR__) . 'assets/css/loteria.js', array(), "1.0", 'all' );
+        }
     }
 
     private function getDataAPI($api_url) {
@@ -67,17 +77,15 @@ class LoteriasPlugin {
         if ($concurso === 'latest') {
 
             $getTransientLoterias = get_transient($transient_loterias);
-
+            
             if ($getTransientLoterias === false) {
                 $getTransientLoterias = $this->getDataAPI($api_url);
-                $time_transient = strtotime($getTransientLoterias['dataProximoConcurso']) - strtotime($getTransientLoterias['data']);
-                set_transient($transient_loterias, json_encode($getTransientLoterias), $time_transient);
-                $this->data_post = $checkPost->check($getTransientLoterias) ===  false ? get_post_field('post_content',$insertPost->add($getTransientLoterias)) : $getTransientLoterias ;
+                $time_transient = strtotime($getTransientLoterias['dataProximoConcurso']) - strtotime("today");
+                set_transient($transient_loterias, json_encode($getTransientLoterias), $time_transient > $this->default_time_transient ? $time_transient : $this->default_time_transient);
+                $this->data_post = $checkPost->check($getTransientLoterias) ===  false ? get_post_field('post_content',$insertPost->add($getTransientLoterias)) : json_encode($getTransientLoterias) ;
             
             } else {
-                
                 return $getTransientLoterias;
-                
             }
             
         } else {
