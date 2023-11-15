d31a28ecbefdadc7310baf4e383559262af9c82f
Edson Araujo (Mike) <edsomichael@gmail.com>
2023-11-14 22:11:58 -0300

    Update javascript admin

--- assets/admin/js/loteria-button.js
+++ assets/admin/js/loteria-button.js
@@ -1,116 +1,82 @@
-(function() {
+(function () {
 
-    const switchModal = () => {
-        const modal = document.querySelector('.modalLoteriasPlugin')
-        const actualStyle = modal.style.display
-        if(actualStyle == 'block') {
-          modal.style.display = 'none'
-        }
-        else {
-          modal.style.display = 'block'
-        }
+  const createModalElement = (type, properties) => {
+    const element = document.createElement(type);
+    Object.assign(element, properties);
+    return element;
+  }
+
+  const divModalLoterias = createModalElement('div', { classList: ['modalLoteriasPlugin'] });
+  const divModalLoteriasContent = createModalElement('div', { classList: ['content'] });
+  const divModalLoteriasValidate = createModalElement('span', { innerText: 'Selecionar loteria, numero ou último concurso' });
+  const divModalLoteriasHeader = createModalElement('h3', { innerText: 'Loterias' });
+  const modalLoteriasSelect = createModalElement('select', { id: 'modal_loteria_select' });
+
+  selectLoterias.forEach(element => {
+      const option = createModalElement('option', { value: element, text: element });
+      modalLoteriasSelect.add(option, null);
+  });
+
+  const divModalLoteriasselect = createModalElement('div', { innerText: 'Selecione o tipo:' });
+  divModalLoteriasselect.appendChild(modalLoteriasSelect);
+
+  const divModalLoteriasseCheckbox = createModalElement('div');
+  const divModalLoteriasCheckbox = createModalElement('input', { type: 'checkbox', id: 'modal_loteria_checkbox' });
+  const divModalLoteriasCheckboxLabel = createModalElement('label', { innerText: 'Exibir último concurso', htmlFor: 'modal_loteria_checkbox' });
+  divModalLoteriasseCheckbox.append(divModalLoteriasCheckbox, divModalLoteriasCheckboxLabel);
+
+  const divModalLoteriasConcursoSeparador = createModalElement('div', { classList: ['separador'] });
+
+  const divModalLoteriasseNumber = createModalElement('div', { innerText: 'Nº do concurso:' });
+  const divModalLoteriasConcurso = createModalElement('input', { type: 'number', id: 'modal_loteria_concurso' });
+  divModalLoteriasseNumber.append(divModalLoteriasConcurso);
+
+  const divModalLoteriasSubmit = createModalElement('input', { type: 'submit', value: 'Inserir', id: 'modal_loteria_submit' });
+
+  divModalLoterias.appendChild(divModalLoteriasContent);
+  divModalLoteriasContent.append(divModalLoteriasValidate, divModalLoteriasHeader, divModalLoteriasselect, divModalLoteriasseCheckbox, divModalLoteriasConcursoSeparador, divModalLoteriasseNumber, divModalLoteriasSubmit);
+
+  document.body.appendChild(divModalLoterias);
+
+  const modal = document.querySelector('.modalLoteriasPlugin');
+  const loteriaSelect = document.querySelector('#modal_loteria_select');
+  const checkbox = document.querySelector('#modal_loteria_checkbox');
+  const concursoInput = document.querySelector('#modal_loteria_concurso');
+  const errorSpan = document.querySelector('.modalLoteriasPlugin span');
+
+  const switchModal = () => {
+      modal.style.display = (modal.style.display === 'block') ? 'none' : 'block';
+  }
+
+  const validateLoterias = () => {
+      const loteria = loteriaSelect.value;
+      const concurso = checkbox.checked ? 'ultimo' : concursoInput.value;
+      const shortcode = `[loteria tipo_concurso="${loteria}" concurso="${concurso}"]`;
+
+      if (loteria && concurso) {
+          tinymce.activeEditor.execCommand('mceInsertContent', false, shortcode);
+          switchModal();
+      } else {
+          errorSpan.classList.add('error');
+          setTimeout(() => errorSpan.classList.remove('error'), 3000);
       }
-    
-    const validateLoterias = () => {
-        const loteria = document.querySelector('#modal_loteria_select').value;
-        const checkbox = document.querySelector('#modal_loteria_checkbox').checked;
-        const concurso = checkbox ? 'ultimo' : document.querySelector('#modal_loteria_concurso').value;
-        const shortcode = `[loteria tipo_concurso="${loteria}" concurso="${concurso}"]`;
-        
-        if (loteria !== '' && concurso !== '' && concurso !== null) {
-            tinymce.activeEditor.execCommand('mceInsertContent', false, shortcode);
-            switchModal();
-        } else {
-            document.querySelector('.modalLoteriasPlugin span').classList.add('error');
-            setTimeout(() => {
-                document.querySelector('.modalLoteriasPlugin span').classList.remove('error');
-            }, 3000);
-        }
-    }
-      
-      window.onclick = function(event) {
-          const modal = document.querySelector('.modalLoteriasPlugin')
-        if (event.target == modal) {
-          switchModal()
-        }
+  }
+
+  window.onclick = (event) => {
+      if (event.target === modal) {
+          switchModal();
       }
-
-    var divModalLoterias = document.createElement("div");
-    divModalLoterias.classList.add("modalLoteriasPlugin");
-
-    var divModalLoteriasContent = document.createElement("div");
-    divModalLoteriasContent.classList.add("content");
-    divModalLoterias.appendChild(divModalLoteriasContent);
-
-    var divModalLoteriasValidate = document.createElement("span");
-    divModalLoteriasValidate.innerText = "Selecionar loteria, numero ou último concurso";
-    divModalLoteriasContent.appendChild(divModalLoteriasValidate);
-
-    var divModalLoteriasHeader = document.createElement("h3");
-    divModalLoteriasHeader.innerText = "Loterias";
-    divModalLoteriasContent.appendChild(divModalLoteriasHeader);
-
-    const modalLoteriasSelect = document.createElement("select");
-    modalLoteriasSelect.id = "modal_loteria_select";
-
-    selectLoterias.forEach(element => {
-
-        var select = document.createElement("option");
-        select.value = element;
-        select.text = element;
-        modalLoteriasSelect.add(select, null);
-        
-    });
-
-    var divModalLoteriasselect = document.createElement("div");
-    divModalLoteriasselect.innerText = "Selecione o tipo:";
-    divModalLoteriasselect.appendChild(modalLoteriasSelect);
-    divModalLoteriasContent.appendChild(divModalLoteriasselect);
-
-    var divModalLoteriasseCheckbox = document.createElement("div");
-    divModalLoteriasContent.appendChild(divModalLoteriasseCheckbox);
-   
-    var divModalLoteriasCheckbox = document.createElement("input");
-    divModalLoteriasCheckbox.type = "checkbox";
-    divModalLoteriasCheckbox.id = "modal_loteria_checkbox";
-    divModalLoteriasseCheckbox.appendChild(divModalLoteriasCheckbox);
-
-    var divModalLoteriasCheckboxLabel = document.createElement("label");
-    divModalLoteriasCheckboxLabel.innerText = "Exibir último concurso";
-    divModalLoteriasCheckboxLabel.htmlFor = "modal_loteria_checkbox";
-    divModalLoteriasseCheckbox.appendChild(divModalLoteriasCheckboxLabel);
+  }
 
 
-    var divModalLoteriasConcursoSeparador = document.createElement("div");
-    divModalLoteriasConcursoSeparador.classList.add("separador");
-    divModalLoteriasContent.appendChild(divModalLoteriasConcursoSeparador);
+  const btn = document.querySelector('#modal_loteria_submit');
+  btn.addEventListener('click', validateLoterias);
 
-    var divModalLoteriasseNumber = document.createElement("div");
-    divModalLoteriasseNumber.innerText = "Nº do concurso:";
-    divModalLoteriasContent.appendChild(divModalLoteriasseNumber);
-
-    var divModalLoteriasConcurso = document.createElement("input");
-    divModalLoteriasConcurso.type = "number";
-    divModalLoteriasConcurso.id = "modal_loteria_concurso";
-    divModalLoteriasseNumber.appendChild(divModalLoteriasConcurso);
-
-    var divModalLoteriasSubmit = document.createElement("input");
-    divModalLoteriasSubmit.type = "submit";
-    divModalLoteriasSubmit.value = "Inserir";
-    divModalLoteriasSubmit.id = "modal_loteria_submit";
-    divModalLoteriasContent.appendChild(divModalLoteriasSubmit);
-
-    document.body.appendChild(divModalLoterias);
-
-    const btn = document.querySelector('#modal_loteria_submit');
-    btn.addEventListener('click', validateLoterias)
-      
-
-    tinymce.PluginManager.add('loteria_button', function(editor, url) {
-        editor.addButton('loteria_button', {
-            text: 'Inserir Loteria',
-            icon: 'loteria-icon',
-            onclick: switchModal
-        });
-    });
+  tinymce.PluginManager.add('loteria_button', function (editor, url) {
+      editor.addButton('loteria_button', {
+          text: 'Inserir Loteria',
+          icon: 'loteria-icon',
+          onclick: switchModal
+      });
+  });
 })();
