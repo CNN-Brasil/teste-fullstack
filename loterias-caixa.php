fed1a0e99f23e0e2f7cff5f2f11e1ee2e6a9e064
Henrique Oliveira <hrq1408@gmail.com>
2024-02-17 14:09:21 -0300

    Configurações basicas do plugin

---
+++ loterias-caixa.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Loterias Caixa - CNN
+Description: Exibir os resultados das loterias da Caixa Econômica Federal
+Version: 1.0
+Author: Henrique Oliveira
+*/
+
+function ativar_loterias_caixa()
+{
+}
+
+register_activation_hook(__FILE__, 'ativar_loterias_caixa');
