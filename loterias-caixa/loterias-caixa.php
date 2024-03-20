3c56497dcee1a8aab9fffdee319bc6b0dc455f26
Eduardo Moraes <adomoraes@gmail.com>
2024-03-20 10:00:33 -0300

    Handling data response

--- loterias-caixa/loterias-caixa.php
+++ loterias-caixa/loterias-caixa.php
@@ -27,16 +27,35 @@ function loterias_caixa_shortcode($atts) {
     $response = wp_remote_get($url, $args);
 
     //DEBUG
-    echo '<pre>';
-    var_dump(wp_remote_retrieve_body($response));
-    echo '</pre>';
+    // echo '<pre>';
+    // var_dump(wp_remote_retrieve_body($response));
+    // echo '</pre>';
 
-    $html = '<div class="loterias-caixa">';
-    $html .= '<h2>Resultados das Loterias CAIXA</h2>';
-    $html .= '<p>Loteria:</p>' . $loteria;
-    $html .= '<p>Concurso:</p>' . $concurso;
-    $html .= '</div>';
+    $dados = json_decode(wp_remote_retrieve_body($response));
+    
+    //DEBUG
+    //var_dump($dados);
 
+        //Valida loteria
+        if ($atts['loteria'] !== '0') {
+
+            $html = '<div>';
+            $html .= '<h2>Resultado do Concurso ' . $dados->concurso . ' da ' . $dados->loteria . '</h2>';
+            $html .= '<p>Data: ' . $dados->data . '</p>';
+            $html .= '<p>Local: ' . $dados->local . '</p>';
+            $html .= '<p>Dezenas Sorteadas: ' . implode(', ', $dados->dezenasOrdemSorteio) . '</p>';
+            $html .= '<p>Premiações:</p>';
+            $html .= '<ul><li>PREMIAÇÕES</li></ul>';
+            $html .= '<p>Valor Arrecadado: R$ ' . number_format($dados->valorArrecadado, 2, ',', '.') . '</p>';
+            $html .= '<p>Acumulou: ' . ($dados->acumulou ? 'Sim' : 'Não') . '</p>';
+            $html .= '<p>Próximo Concurso: ' . $dados->proximoConcurso . ' - Data: ' . $dados->dataProximoConcurso . '</p>';
+            $html .= '<p>Valor Estimado do Próximo Concurso: R$ ' . number_format($dados->valorEstimadoProximoConcurso, 2, ',', '.') . '</p>';
+            $html .= '</div>';
+        } else {
+            $html = '<div class="loterias-caixa">';
+            $html .= '<p>Por favor, especifique o número do concurso usando o parâmetro "concurso".</p>';
+            $html .= '</div>';
+        }
     return $html;
 }
 add_shortcode('loterias_caixa', 'loterias_caixa_shortcode');
