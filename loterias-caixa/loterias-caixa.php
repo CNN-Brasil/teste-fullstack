1753c8aa5b97ce5e62064f4d07f454899dc51dbb
Eduardo Moraes <adomoraes@gmail.com>
2024-03-20 09:25:38 -0300

    Getting attributes of shortcode

--- loterias-caixa/loterias-caixa.php
+++ loterias-caixa/loterias-caixa.php
@@ -8,10 +8,20 @@ Author: Eduardo Moraes
 Author URI: https://github.com/adomoraes/
 */
 
-function loterias_caixa_shortcode() {
+function loterias_caixa_shortcode($atts) {
+
+    $atts = shortcode_atts(array(
+        'loteria' => '0',
+        'concurso' => '0',
+    ), $atts);
+
+    $loteria = $atts['loteria'];
+    $concurso = $atts['concurso'];
 
     $html = '<div class="loterias-caixa">';
     $html .= '<h2>Resultados das Loterias CAIXA</h2>';
+    $html .= '<p>Loteria:</p>' . $loteria;
+    $html .= '<p>Concurso:</p>' . $concurso;
     $html .= '</div>';
 
     return $html;
